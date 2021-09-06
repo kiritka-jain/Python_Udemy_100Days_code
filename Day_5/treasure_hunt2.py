a7bf2e0667d00c7ce51cbd2ace798866359387ee
Kirtika Jain <kirti@Saurabhs-MacBook-Air.local>
2021-09-06 13:15:30 +0530

    add 4 new programs

---
+++ Day_5/treasure_hunt2.py
@@ -0,0 +1,9 @@
+row_1 = [" "," "," "]
+row_2 = [" "," "," "]
+row_3 = [" "," "," "]
+treasure_map  = [row_1,row_2,row_3]
+print(f"row_1\nrow_2\nrow_3")
+place_val = input("enter the place where you want to place the 'x'")
+row_val = int(place_val[0])
+column_val = int(place_val[1])
+
