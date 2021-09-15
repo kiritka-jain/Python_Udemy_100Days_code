9528b7abe4fb7b220911970b2f1034211c105704
kiritka-jain <58281851+kiritka-jain@users.noreply.github.com>
2021-09-15 18:00:47 +0530

    Day7 (#2)
    * add gitignore file
    
    * add hangman game project day7
    
    * add ascii art module and words list
    
    * Delete Words_list.cpython-39.pyc
    
    * Delete hangman_ascii_art.cpython-39.pyc
    
    Co-authored-by: Kirtika Jain <kirti@Saurabhs-MacBook-Air.local>

---
+++ Day_7/hangman.py
@@ -0,0 +1,44 @@
+import random
+import Words_list
+import hangman_ascii_art
+
+
+word = random.choice(Words_list.word_list)
+word_len = len(word)
+blanks = []
+matched_letter = ''
+life_lines = 6
+chances = 0
+hangman_index = 1
+for _ in range(word_len):
+    blanks.append("_")
+print(blanks)
+
+game_over = False
+
+
+def letter_matching(player_letter):
+    for i in range(word_len):
+        if player_letter == word[i]:
+            blanks[i] = player_letter
+    return blanks
+
+
+while chances < word_len and life_lines > 0:
+    if '_' not in blanks:
+        game_over = True
+        print("Game Over!")
+        break
+    else:
+        player_letter = input("Enter a letter.").lower()
+        chances += 1
+        if player_letter not in word:
+            life_lines -= 1
+            print(hangman_ascii_art.HANGMANPICS[hangman_index])
+            hangman_index += 1
+        matched_letter = letter_matching(player_letter)
+        print(matched_letter)
+if '_' not in matched_letter:
+    print("You Win!:")
+else:
+    print('You Lose!')
