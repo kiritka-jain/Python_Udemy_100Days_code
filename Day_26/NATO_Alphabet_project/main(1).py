dab1f170dfe68ebd9f83f2e81fee8930c14f0905
Kirtika Jain <kirti@Saurabhs-MacBook-Air.local>
2021-10-21 16:36:03 +0530

    modified NATO Alphabet project with keyerror handling.

--- Day_26/NATO_Alphabet_project/main(1).py
+++ Day_26/NATO_Alphabet_project/main(1).py
@@ -3,7 +3,14 @@ import pandas
 nato_states = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_series = {rows.letter:rows.code for (index,rows) in nato_states.iterrows()}
 
+def generate_phonetic():
+    entered_word = input("PLease enter a word:").upper()
+    try:
+        result =[alphabet_series[letter] for letter in entered_word]
+    except KeyError:
+        print("Sorry, only letters in alphabet please.")
+        generate_phonetic()
+    else:
+        print(result)
 
-entered_word = input("PLease enter a word:").upper()
-result =[alphabet_series[letter] for letter in entered_word]
-print(result)
+generate_phonetic()
